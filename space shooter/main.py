6461f06eca96188907d05f203d8f9f591a0b67eb
panjiarga <nandaekaputra@outlook.com>
2024-06-04 22:08:19 +0100

    updated with class player and star

--- space shooter/main.py
+++ space shooter/main.py
@@ -2,6 +2,57 @@ import pygame
 from random import randint
 from os.path import join
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join(FILEPATH,'images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+        self.direction = pygame.Vector2(0,0)
+        self.speed = 500
+
+    def update(self,dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print('fire laser')
+        # player
+        p.rect.center += p.direction * p.speed * dt
+        if p.rect.right > WINDOW_WIDTH:
+            p.rect.right = WINDOW_WIDTH
+        if p.rect.left < 0:
+            p.rect.left = 0
+        if p.rect.top < 0:
+            p.rect.top = 0
+        if p.rect.bottom > WINDOW_HEIGHT:
+            p.rect.bottom = WINDOW_HEIGHT
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, speed, groups):
+        super().__init__(groups)
+        self.speed = speed
+        self.image = pygame.image.load(join(FILEPATH,'images','star.png')).convert_alpha()
+        self.rect = self.image.get_frect(center=(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)))
+        
+
+    def update(self,dt):
+        # linear star
+        self.rect.y += (self.speed*dt)
+        if self.rect.bottom > WINDOW_HEIGHT:
+            self.rect.top = 0
+            self.rect.x = randint(0,WINDOW_WIDTH)
+        
+
+        # stochastic star   
+        # for i in range(5):
+        #     xrand = randint(0,WINDOW_WIDTH)
+        #     yrand = randint(0,WINDOW_HEIGHT)
+        #     display_surface.blit(self.image, (xrand,yrand)) 
+
+
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
@@ -11,21 +62,11 @@ running = True
 clock = pygame.time.Clock()
 FILEPATH = 'space shooter'
 
-# surface
-
-# converting image, if no transparent pixels: convert(), if has transparent pixels convert_alpha()
-player_surf = pygame.image.load(join(FILEPATH,'images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
-x_player = 100
-player_direction = pygame.math.Vector2(0,0)
-player_speed = 500
-
-star_amount = 20
-star_surf = pygame.image.load(join(FILEPATH,'images','star.png')).convert_alpha()
-star_positions = [[randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)] for i in range(star_amount)]
-x_star = randint(0,1280)
-y_star = randint(0,720)
-
+# initialization
+all_sprites = pygame.sprite.Group()
+p = Player(all_sprites)
+for i in range(20):
+    s = Star(p.speed,all_sprites)
 
 meteor_surf = pygame.image.load(join(FILEPATH,'images','meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
@@ -39,68 +80,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # enable quit
             running = False
-        #if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-        #    player_speed=player_speed*0.5
-        #else:
-        #    player_speed=500
-        #   player_rect.bottom = WINDOW_HEIGHT
-    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_DOWN]:
-        player_rect.y += player_speed * dt
-        if player_speed>=300:
-            player_speed -= 1
-    else:
-        player_speed = 500
-    if keys[pygame.K_RIGHT]:
-        player_direction.x = 1
-    if keys[pygame.K_LEFT]:
-        player_direction.x = -1
-    if keys[pygame.K_UP]:
-        player_rect.y -= player_speed * dt    
+    all_sprites.update(dt)
 
     # draw the game, order matters, last will be top
-    display_surface.fill('cyan4')
-    for pos in star_positions:
-        display_surface.blit(star_surf, (pos[0],pos[1])) 
+    display_surface.fill('black') 
 
-        # star_positions[] = [(pos[0]+1,pos[1]+1)]
-        # x_star = random.randint(0,WINDOW_WIDTH)
-        # y_star = random.randint(0,WINDOW_HEIGHT)
+    for i in range(2):
+        display_surface.blit(meteor_surf, (randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)))
     
-    for i in range(star_amount):
-        star_positions[i][1] += (player_speed*dt)
-        if star_positions[i][1] > WINDOW_HEIGHT:
-            star_positions[i][1] = 0
-            star_positions[i][0] = randint(0,WINDOW_WIDTH)
-        
-        #for i in range(5):
-    #    x_star = randint(0,WINDOW_WIDTH)
-    #    y_star = randint(0,WINDOW_HEIGHT)
-    #    display_surface.blit(star_surf, (x_star,y_star)) 
+    all_sprites.draw(display_surface)
 
-    display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(laser_surf, laser_rect)
-
-    #if (player_rect.bottom >= WINDOW_HEIGHT) or (player_rect.top <= 0):
-    #   player_direction.y *= -1
-    #    player_rect.bottom = WINDOW_HEIGHT
-    #if (player_rect.left <= 0) or (player_rect.right >= WINDOW_WIDTH): 
-    #    player_direction.x *= -1
-    
-        
-    player_rect.center += player_direction * player_speed * dt
-    if player_rect.right > WINDOW_WIDTH:
-        player_rect.right = WINDOW_WIDTH
-        player_direction *= -1
-    if player_rect.left < 0:
-        player_rect.left = 0
-        player_direction *= -1
-    display_surface.blit(player_surf, player_rect)
    
     
     pygame.display.update() # flip() update some part of the window
     
-
-
 pygame.quit()
